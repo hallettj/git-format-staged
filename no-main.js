b8cdd9323b2feee7580732233146ed5a6a69e47f
Test Person <test@test.com>
2018-05-23 14:17:27 -0400

    There is no main module

---
+++ no-main.js
@@ -0,0 +1,5 @@
+'use strict'
+var message =
+  'It looks like you are trying to import `git-format-staged`. This module does not export a Javascript library - it is just a CLI program.'
+console.info(message)
+module.exports = message
