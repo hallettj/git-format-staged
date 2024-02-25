92856b8e8d6d360f773b7894c5a1950cd2486771
Jesse Hallett <jesse@sitr.us>
2024-02-25 14:52:12 -0800

    build: add flake.nix

---
+++ git-format-staged.nix
@@ -0,0 +1,18 @@
+# Nix packaging for git-format-staged
+{ stdenvNoCC
+, python3
+}:
+let
+  manifest = builtins.fromJSON (builtins.readFile ./package.json);
+in
+stdenvNoCC.mkDerivation {
+  name = "git-format-staged";
+  version = manifest.version;
+  src = ./.;
+  buildInputs = [ python3 ];
+  installPhase = ''
+    mkdir -p "$out/bin"
+    cp "$src/git-format-staged" "$out/bin/"
+    chmod +x "$out/bin/git-format-staged"
+  '';
+}
